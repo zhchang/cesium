11fac570ede4ce0298e49e5831be53ae4c75c490
zhchang <zhchang@gmail.com>
2017-01-05 15:21:10 +0800

    rename pkg

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-    name='cesium-grpc',
-    packages=['cesium-grpc'],  # this must be the same as the name above
-    version='0.3',
+    name='cesium_grpc',
+    packages=['cesium_grpc'],  # this must be the same as the name above
+    version='0.4',
     description='a grpc code generator',
     author='chang',
     author_email='zhchang@gmail.com',
     url='https://github.com/zhchang/cesium',  # use the URL to the github repo
     # I'll explain this in a second
-    download_url='https://github.com/zhchang/cesium/tarball/0.3',
+    download_url='https://github.com/zhchang/cesium/tarball/0.4',
     keywords=['python', 'grpc', 'code gen'],  # arbitrary keywords
     classifiers=[],
 )

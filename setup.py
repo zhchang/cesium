ca35aa6864bd3c8ec026c8b1798750b21cf183a2
zhchang <zhchang@gmail.com>
2017-01-05 16:48:19 +0800

    add wrapper

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 setup(
     name='cesium_grpc',
     packages=['cesium_grpc'],  # this must be the same as the name above
-    version='0.6',
+    version='0.7',
     description='a grpc code generator',
     author='chang',
     author_email='zhchang@gmail.com',
     url='https://github.com/zhchang/cesium',  # use the URL to the github repo
     # I'll explain this in a second
-    download_url='https://github.com/zhchang/cesium/tarball/0.6',
+    download_url='https://github.com/zhchang/cesium/tarball/0.7',
     keywords=['python', 'grpc', 'code gen'],  # arbitrary keywords
     classifiers=[],
+    data_files=['/usr/local/bin', ['cesium']],
 )

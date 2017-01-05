d8084717b8dd018880414f01fab8281179bb85b0
zhchang <zhchang@gmail.com>
2017-01-05 14:59:12 +0800

    setup pypi structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='cesium',
+    packages=['cesium'],  # this must be the same as the name above
+    version='0.1',
+    description='a grpc code generator',
+    author='chang',
+    author_email='zhchang@gmail.com',
+    url='https://github.com/zhchang/cesium',  # use the URL to the github repo
+    # I'll explain this in a second
+    download_url='https://github.com/zhchang/cesium/tarball/0.1',
+    keywords=['python', 'grpc', 'code gen'],  # arbitrary keywords
+    classifiers=[],
+)

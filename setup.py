e73039dbbe41cd1a58060ffd03f25983e47c6099
zhchang <zhchang@gmail.com>
2017-01-05 18:32:09 +0800

    update rls

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
-ver = '0.94'
+ver = '0.95'
 setup(
     name='cesium_grpc',
     packages=['cesium_grpc'],  # this must be the same as the name above
@@ -11,6 +11,6 @@ setup(
     # I'll explain this in a second
     download_url='https://github.com/zhchang/cesium/tarball/' + ver,
     keywords=['python', 'grpc', 'code gen'],  # arbitrary keywords
-    scripts=['scripts/add_wrapper'],
+    scripts=['scripts/cesium'],
     classifiers=[],
 )
